b1394c531a5d2b849e165e638cb7bd2f1e07a4c0
rianbrob <rianbrob@hotmail.com>
2021-05-14 14:59:35 -0400

    web build

---
+++ precache-manifest.9aff5fd1e992a469561dd08719e74d90.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aef8fe82704c563bf52f9b36ee39f24",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a4f67e130e6a400e69f",
+    "url": "/static/css/2.13c6718b.chunk.css"
+  },
+  {
+    "revision": "10f7ddba1fca0042d81f",
+    "url": "/static/css/main.00770f9f.chunk.css"
+  },
+  {
+    "revision": "2a4f67e130e6a400e69f",
+    "url": "/static/js/2.cc21819c.chunk.js"
+  },
+  {
+    "revision": "870e628bab9b77a4ef09324bc173f02d",
+    "url": "/static/js/2.cc21819c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10f7ddba1fca0042d81f",
+    "url": "/static/js/main.b3430d7c.chunk.js"
+  },
+  {
+    "revision": "e7f30327dd9cd1ffaa97",
+    "url": "/static/js/runtime-main.30ea0096.js"
+  },
+  {
+    "revision": "1c251c8ff2222207650b4f24a2159cbb",
+    "url": "/static/media/dot-pattern.1c251c8f.svg"
+  },
+  {
+    "revision": "5e030c93ee64514b9fcdc887289ce8a9",
+    "url": "/static/media/facebook-icon.5e030c93.svg"
+  },
+  {
+    "revision": "61eae8b2845b7d35c3d58786733ea3d2",
+    "url": "/static/media/github-icon.61eae8b2.svg"
+  },
+  {
+    "revision": "fa5666fa9730a9f927a76edfa9b53e2b",
+    "url": "/static/media/linkedin-icon.fa5666fa.svg"
+  },
+  {
+    "revision": "f9776f55173cd40b2b3f3fed1c7443c4",
+    "url": "/static/media/logo.f9776f55.svg"
+  },
+  {
+    "revision": "ca394ec8a4754ab9db293dcac626bdcb",
+    "url": "/static/media/menu.ca394ec8.svg"
+  },
+  {
+    "revision": "ee7355f758d41f9d4421d9c7baf8a1b9",
+    "url": "/static/media/nook.ee7355f7.png"
+  },
+  {
+    "revision": "297d50a2cd8b864f0c1f357e5108d87a",
+    "url": "/static/media/shield-icon.297d50a2.svg"
+  },
+  {
+    "revision": "5881d2b0ea1f25533b3449a86aa09282",
+    "url": "/static/media/sponge.5881d2b0.png"
+  },
+  {
+    "revision": "a200bad45b923b0b206917b16755f83f",
+    "url": "/static/media/stats-illustration.a200bad4.svg"
+  },
+  {
+    "revision": "159be2476eedd8b63bb0c7e0a58d0e47",
+    "url": "/static/media/svg-decorator-blob-3.159be247.svg"
+  },
+  {
+    "revision": "7189052ca672d113f057d9a4f3cb5efe",
+    "url": "/static/media/twitter-icon.7189052c.svg"
+  },
+  {
+    "revision": "b0200bf7cbbb26c32989e7a0e4d19eef",
+    "url": "/static/media/x.b0200bf7.svg"
+  }
+]);
